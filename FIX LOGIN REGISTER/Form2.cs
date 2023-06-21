e0f08dc61e229c8767a98b23bd533abe2992f328
ramamaul76 <ramamaul76@gmail.com>
2023-06-21 08:27:37 +0700

    Login Register Forgot Password

---
+++ FIX LOGIN REGISTER/Form2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIX_LOGIN_REGISTER
+{
+    public partial class Form2 : Form
+    {
+        private Login.User user;
+        public Form2(Login.User user)
+        {
+            InitializeComponent();
+            this.user = user;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ FIX LOGIN REGISTER/Form3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIX_LOGIN_REGISTER
+{
+    public partial class Form3 : Form
+    {
+        private Login.User user;
+        public Form3(Login.User user)
+        {
+            InitializeComponent();
+            this.user = user;
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
