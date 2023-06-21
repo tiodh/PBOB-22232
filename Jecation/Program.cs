3f1d2a90f8ef4c14aabf941644a63c53571710a1
Azmi010 <ulul10039@gmail.com>
2023-06-21 11:29:10 +0700

    Revisi pertama

--- Jecation/Program.cs
+++ Jecation/Program.cs
@@ -1,4 +1,4 @@
-namespace Jecation
+namespace FIX_LOGIN_REGISTER
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace Jecation
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
