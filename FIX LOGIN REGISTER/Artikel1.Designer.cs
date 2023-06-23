58463baf265ba449df6f69b77583977adc05f507
niakurnia54 <niakurniaila@gmail.com>
2023-06-24 01:11:39 +0700

    Frontend Artikel

---
+++ FIX LOGIN REGISTER/Artikel1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Artikeldesain
+{
+    partial class BgLabel
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnJudul = new System.Windows.Forms.Button();
+            this.pcboxGambarRembangan = new System.Windows.Forms.PictureBox();
+            this.btnartikelterkait = new System.Windows.Forms.Button();
+            this.btnArtikelyangTerkait = new System.Windows.Forms.Button();
+            this.btnArtikel = new System.Windows.Forms.Button();
+            this.btnReadMore = new System.Windows.Forms.Button();
+            this.BtnSearchArtikel = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcboxGambarRembangan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2179, 499);
+            this.button1.Margin = new System.Windows.Forms.Padding(19, 20, 19, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(0, 0);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 68);
+            this.button2.Margin = new System.Windows.Forms.Padding(19, 20, 19, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(0, 0);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Artikel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnJudul
+            // 
+            this.btnJudul.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(102)))), ((int)(((byte)(255)))));
+            this.btnJudul.ForeColor = System.Drawing.Color.White;
+            this.btnJudul.Location = new System.Drawing.Point(-9, -20);
+            this.btnJudul.Name = "btnJudul";
+            this.btnJudul.Size = new System.Drawing.Size(1495, 143);
+            this.btnJudul.TabIndex = 2;
+            this.btnJudul.Text = "Artikel";
+            this.btnJudul.UseVisualStyleBackColor = false;
+            // 
+            // pcboxGambarRembangan
+            // 
+            this.pcboxGambarRembangan.Image = global::Artikeldesain.Properties.Resources.download__5_;
+            this.pcboxGambarRembangan.Location = new System.Drawing.Point(-9, 113);
+            this.pcboxGambarRembangan.Name = "pcboxGambarRembangan";
+            this.pcboxGambarRembangan.Size = new System.Drawing.Size(1557, 381);
+            this.pcboxGambarRembangan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcboxGambarRembangan.TabIndex = 3;
+            this.pcboxGambarRembangan.TabStop = false;
+            this.pcboxGambarRembangan.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // btnartikelterkait
+            // 
+            this.btnartikelterkait.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnartikelterkait.ForeColor = System.Drawing.Color.Silver;
+            this.btnartikelterkait.Location = new System.Drawing.Point(63, 420);
+            this.btnartikelterkait.Name = "btnartikelterkait";
+            this.btnartikelterkait.Size = new System.Drawing.Size(378, 169);
+            this.btnartikelterkait.TabIndex = 4;
+            this.btnartikelterkait.Text = "ISI";
+            this.btnartikelterkait.UseVisualStyleBackColor = true;
+            // 
+            // btnArtikelyangTerkait
+            // 
+            this.btnArtikelyangTerkait.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnArtikelyangTerkait.ForeColor = System.Drawing.Color.Silver;
+            this.btnArtikelyangTerkait.Location = new System.Drawing.Point(927, 420);
+            this.btnArtikelyangTerkait.Name = "btnArtikelyangTerkait";
+            this.btnArtikelyangTerkait.Size = new System.Drawing.Size(378, 169);
+            this.btnArtikelyangTerkait.TabIndex = 5;
+            this.btnArtikelyangTerkait.Text = "ISI";
+            this.btnArtikelyangTerkait.UseVisualStyleBackColor = true;
+            this.btnArtikelyangTerkait.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnArtikel
+            // 
+            this.btnArtikel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnArtikel.ForeColor = System.Drawing.Color.Silver;
+            this.btnArtikel.Location = new System.Drawing.Point(489, 420);
+            this.btnArtikel.Name = "btnArtikel";
+            this.btnArtikel.Size = new System.Drawing.Size(378, 169);
+            this.btnArtikel.TabIndex = 6;
+            this.btnArtikel.Text = "ISI";
+            this.btnArtikel.UseVisualStyleBackColor = true;
+            this.btnArtikel.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btnReadMore
+            // 
+            this.btnReadMore.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(102)))), ((int)(((byte)(255)))));
+            this.btnReadMore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnReadMore.ForeColor = System.Drawing.Color.Black;
+            this.btnReadMore.Location = new System.Drawing.Point(1023, 629);
+            this.btnReadMore.Name = "btnReadMore";
+            this.btnReadMore.Size = new System.Drawing.Size(170, 44);
+            this.btnReadMore.TabIndex = 7;
+            this.btnReadMore.Text = "Read More ...";
+            this.btnReadMore.UseVisualStyleBackColor = false;
+            this.btnReadMore.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // BtnSearchArtikel
+            // 
+            this.BtnSearchArtikel.BackColor = System.Drawing.Color.LightGray;
+            this.BtnSearchArtikel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnSearchArtikel.ForeColor = System.Drawing.Color.Black;
+            this.BtnSearchArtikel.Location = new System.Drawing.Point(886, 129);
+            this.BtnSearchArtikel.Name = "BtnSearchArtikel";
+            this.BtnSearchArtikel.Size = new System.Drawing.Size(351, 44);
+            this.BtnSearchArtikel.TabIndex = 8;
+            this.BtnSearchArtikel.Text = "Search ...";
+            this.BtnSearchArtikel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnSearchArtikel.UseVisualStyleBackColor = false;
+            this.BtnSearchArtikel.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox3.Image = global::Artikeldesain.Properties.Resources.icons8_search_30;
+            this.pictureBox3.Location = new System.Drawing.Point(1183, 139);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(45, 30);
+            this.pictureBox3.TabIndex = 36;
+            this.pictureBox3.TabStop = false;
+            // 
+            // BgLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(37F, 73F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1369, 749);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.BtnSearchArtikel);
+            this.Controls.Add(this.btnReadMore);
+            this.Controls.Add(this.btnArtikel);
+            this.Controls.Add(this.btnArtikelyangTerkait);
+            this.Controls.Add(this.btnartikelterkait);
+            this.Controls.Add(this.pcboxGambarRembangan);
+            this.Controls.Add(this.btnJudul);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(102)))), ((int)(((byte)(255)))));
+            this.Margin = new System.Windows.Forms.Padding(19, 20, 19, 20);
+            this.Name = "BgLabel";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pcboxGambarRembangan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button btnJudul;
+        private PictureBox pcboxGambarRembangan;
+        private Button btnartikelterkait;
+        private Button btnArtikelyangTerkait;
+        private Button btnArtikel;
+        private Button btnReadMore;
+        private Button BtnSearchArtikel;
+        private PictureBox pictureBox3;
+    }
+}
---
+++ FIX LOGIN REGISTER/Artikel1.cs
@@ -0,0 +1,55 @@
+namespace Artikeldesain
+{
+    public partial class BgLabel : Form
+    {
+        public BgLabel()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
