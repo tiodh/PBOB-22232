b3aca28bccf433196d85dfc405ce8f85d5b2b46d
Iin mardhiana <iinmardhiana08@gmail.com>
2023-06-23 21:18:50 +0700

    Frontend Homepage Admin

---
+++ FIX LOGIN REGISTER/Profil Admin.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace homepage_admin
+{
+    partial class profil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox4 = new PictureBox();
+            label7 = new Label();
+            label6 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(8, 60);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(48, 50);
+            pictureBox4.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox4.TabIndex = 15;
+            pictureBox4.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Microsoft Sans Serif", 9.749999F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(59, 63);
+            label7.Name = "label7";
+            label7.Size = new Size(112, 16);
+            label7.TabIndex = 10;
+            label7.Text = "Nama Lengkap";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Microsoft Sans Serif", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(12, 12);
+            label6.Name = "label6";
+            label6.Size = new Size(75, 29);
+            label6.TabIndex = 9;
+            label6.Text = "Profil";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DodgerBlue;
+            button1.Font = new Font("Microsoft Sans Serif", 6F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(58, 126);
+            button1.Name = "button1";
+            button1.Size = new Size(52, 25);
+            button1.TabIndex = 16;
+            button1.Text = "Logout";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // profil
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(173, 159);
+            Controls.Add(button1);
+            Controls.Add(pictureBox4);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Name = "profil";
+            Text = "profil";
+            Load += logout_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox4;
+        private Label label7;
+        private Label label6;
+        private Button button1;
+    }
+}
