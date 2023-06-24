7da0a808fe558b8e352cd437ce8309992478146c
ditanzts <ditanztss10@gmail.com>
2023-06-24 19:24:48 +0700

    CRUD Update Database

--- FIX LOGIN REGISTER/updatedaabaseadmin2.cs
+++ FIX LOGIN REGISTER/updatedaabaseadmin2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,24 +8,132 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
-namespace rembangan_1
+namespace updatetiket
 {
-    public partial class updatedaabaseadmin2 : Form
+    public partial class UpHargaTiketANK : Form
     {
-        public updatedaabaseadmin2()
+        public UpHargaTiketANK()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HargaTiket frm = new HargaTiket();
+            frm.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost; Port=5432; Database=jecationn; User Id=postgres; Password=dita041003");
+            conn.Open();
+            NpgsqlCommand comm = conn.CreateCommand();
+            comm.CommandType = CommandType.Text;
+            comm.CommandText = "update tiket_masuk set harga_tiket_masuk = '" + textBox3.Text + "' where id_tiket_masuk = '" + textBox1.Text + "' and kategori = '" + textBox2.Text + "'";
+            comm.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Harga tiket berhasil diperbarui!");
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost; Port=5432; Database=jecationn; User Id=postgres; Password=dita041003");
+            conn.Open();
+            NpgsqlCommand comm = new NpgsqlCommand();
+            comm.Connection = conn;
+            comm.CommandType = CommandType.Text;
+            comm.CommandText = "select * from tiket_masuk where id_tiket_masuk = 1";
+            NpgsqlDataReader reader = comm.ExecuteReader();
+            if (reader.Read())
+
+            {
+                textBox1.Text = reader["id_tiket_masuk"].ToString();
+                textBox2.Text = reader["kategori"].ToString();
+                textBox3.Text = reader["harga_tiket_masuk"].ToString();
+            }
+            conn.Close();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+
+    public partial class UpHargaTiketDWS : Form
+    {
+        public UpHargaTiketDWS()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost; Port=5432; Database=jecationn; User Id=postgres; Password=dita041003");
+            conn.Open();
+            NpgsqlCommand comm = new NpgsqlCommand();
+            comm.Connection = conn;
+            comm.CommandType = CommandType.Text;
+            comm.CommandText = "select * from tiket_masuk where id_tiket_masuk = 2";
+            NpgsqlDataReader reader = comm.ExecuteReader();
+            if (reader.Read())
+
+            {
+                textBox1.Text = reader["id_tiket_masuk"].ToString();
+                textBox2.Text = reader["kategori"].ToString();
+                textBox3.Text = reader["harga_tiket_masuk"].ToString();
+            }
+            conn.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost; Port=5432; Database=jecationn; User Id=postgres; Password=dita041003");
+            conn.Open();
+            NpgsqlCommand comm = conn.CreateCommand();
+            comm.CommandType = CommandType.Text;
+            comm.CommandText = "update tiket_masuk set harga_tiket_masuk = '" + textBox3.Text + "' where id_tiket_masuk = '" + textBox1.Text + "' and kategori = '" + textBox2.Text + "'";
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+            MessageBox.Show("Harga tiket berhasil diperbarui!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HargaTiket frm = new HargaTiket();
+            frm.ShowDialog();
         }
     }
 }
--- FIX LOGIN REGISTER/updatedatabaseadmin.cs
+++ FIX LOGIN REGISTER/updatedatabaseadmin.cs
@@ -1,15 +1,23 @@
-namespace rembangan_1
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace updatetiket
 {
-    public partial class updatedatabaseadmin : Form
+    public partial class HargaTiket : Form
     {
-        public updatedatabaseadmin()
+        public HargaTiket()
         {
             InitializeComponent();
-        }
-
-        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
-        {
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -17,17 +25,85 @@ namespace rembangan_1
 
         }
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection("Server=localhost; Port=5432; Database=jecationn; User Id=postgres; Password=dita041003"))
+            {
+                conn.Open();
+                NpgsqlCommand comm = new NpgsqlCommand();
+                comm.Connection = conn;
+                comm.CommandType = CommandType.Text;
+                comm.CommandText = "select kategori, harga_tiket_masuk from tiket_masuk where id_tiket_masuk = 2";
+                NpgsqlDataReader reader = comm.ExecuteReader();
+                if (reader.Read())
+                {
+                    label4.Text = reader["kategori"].ToString();
+                    label5.Text = reader["harga_tiket_masuk"].ToString();
+                }
+                conn.Close();
+            }
+
+            using (NpgsqlConnection conn = new NpgsqlConnection("Server=localhost; Port=5432; Database=jecationn; User Id=postgres; Password=dita041003"))
+            {
+                conn.Open();
+                NpgsqlCommand comm = new NpgsqlCommand();
+                comm.Connection = conn;
+                comm.CommandType = CommandType.Text;
+                comm.CommandText = "select kategori, harga_tiket_masuk from tiket_masuk where id_tiket_masuk = 1";
+                NpgsqlDataReader reader = comm.ExecuteReader();
+                if (reader.Read())
+                {
+                    label2.Text = reader["kategori"].ToString();
+                    label3.Text = reader["harga_tiket_masuk"].ToString();
+                }
+                conn.Close();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UpHargaTiketANK frm = new UpHargaTiketANK();
+            frm.ShowDialog();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
 
         private void button3_Click(object sender, EventArgs e)
+        {
+            UpHargaTiketDWS frm = new UpHargaTiketDWS();
+            frm.ShowDialog();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
         {
 
         }
