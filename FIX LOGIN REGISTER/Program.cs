706fca4d5be1044970c7a424eac295f0a8f0ff28
RIFKICah <rifkicahyomukti@gmail.com>
2023-06-24 12:43:57 +0700

    FAQ Pertanyaan

--- FIX LOGIN REGISTER/Program.cs
+++ FIX LOGIN REGISTER/Program.cs
@@ -1,4 +1,4 @@
-namespace GantiPassword
+namespace FAQ
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace GantiPassword
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new GantiPassword1());
+            Application.Run(new FAQ());
         }
     }
 }
